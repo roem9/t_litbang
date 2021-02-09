4cf30a484674d90968e55d4259fbbf33d682ee67
roem9 <muhammadrum04@gmail.com>
2021-02-09 22:19:09 +0700

    goo

--- application/models/Kel_model.php
+++ application/models/Kel_model.php
@@ -3,8 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  
 class Kel_model extends CI_Model { 
     var $table = 'kelas_pembinaan';
-    var $column_order = array(null,'a.status',null,null,'a.program',null,'b.nama_kpq',null,null); //set column field database for datatable orderable
-    var $column_search = array('a.status','a.program','b.nama_kpq','hari','tempat'); //set column field database for datatable searchable 
+    var $column_order = array(null,'status',null,null,'program',null,'nama_kpq',null,null); //set column field database for datatable orderable
+    var $column_search = array('status','program','nama_kpq','hari','tempat'); //set column field database for datatable searchable 
     var $order = array('id_kelas' => 'asc'); // default order 
  
     public function __construct()
